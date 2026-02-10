18945cb37004ff6fe71bb162329ef9d5d6fb6f48
Oljake <109909586+Oljake@users.noreply.github.com>
2026-02-10 23:33:55 +0200

    Fix missing newline at end of config.py
    Add missing newline at the end of the file

---
+++ config.py
@@ -0,0 +1,41 @@
+debug_mode = True
+
+fishing_spot_images = ("images/fishing_spot_0.png", "images/fishing_spot_1.png", "images/fishing_spot_2.png", "images/fishing_spot_3.png")
+is_fishing_image = "images/is_fishing.png"
+inventory_images = ("images/fish_0.png", "images/fish_1.png", "images/fish_2.png")
+login_screen_image = "images/login_screen.png"
+click_to_play_image = "images/click_to_play.png"
+loading_image = "images/loading.png"
+
+loading_region = (16, 38, 268, 75)
+login_screen_region = (810, 26, 1724, 357)
+click_login_screen_region = (1064, 499, 1460, 598)
+click_to_play_region = (1048, 620, 1485, 779)
+inventory_region = (2130, 818, 2134, 1440)
+last_inventory_slot_region = (2379, 1249, 2446, 1306)
+is_fishing_region = (15, 34, 2095, 105)
+fishing_spot_region = (0, 0, 2134, 1440)
+conf = 65
+
+breaks = [
+    ('Bathroom break',      (60 , 80 ), (5 , 10)),
+    ('Snack break',         (90 , 120), (10, 15)),
+    ('Stretch break',       (30 , 60 ), (3 , 5 )),
+    ('Water break',         (45 , 75 ), (2 , 5 )),
+    ('Coffee break',        (120, 150), (10, 20)),
+    ('Lunch break',         (180, 240), (30, 60)),
+    ('Walk break',          (120, 180), (15, 25)),
+    ('Meditation break',    (90 , 120), (5 , 10)),
+    ('Phone break',         (60 , 90 ), (5 , 15)),
+    ('Nap break',           (240, 300), (15, 30)),
+    ('Exercise break',      (180, 240), (20, 40)),
+    ('Social break',        (150, 210), (10, 20)),
+    ('Reading break',       (240, 360), (20, 30)),
+    ('Music break',         (60 , 120), (10, 15)),
+    ('Game break',          (240, 300), (30, 45)),
+    ('Cleaning break',      (120, 180), (15, 25)),
+    ('Hobby break',         (180, 240), (20, 40)),
+    ('Reflection break',    (300, 360), (10, 20)),
+    ('Tea break',           (90 , 150), (5 , 10)),
+    ('Breathing break',     (45 , 90 ), (2 , 5 ))
+]
